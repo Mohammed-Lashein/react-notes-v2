5d44ee011bda4f759107e2ae22df2de9afe589d3
Mohammed-Lashein <132823270+Mohammed-Lashein@users.noreply.github.com>
2023-07-01 23:42:30 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import {HashRouter, Routes, Route} from 'react-router-dom';
 import Notes from './pages/Notes'
 import CreateNote from './pages/CreateNote'
 import EditNote from './pages/EditNote'
 const App = () => {
   return (
     <main id="app">
-    <BrowserRouter> 
+    <HashRouter> 
     <Routes>
       <Route path='/' element={<Notes/>} />
       <Route path='/create-note' element={<CreateNote/>} />
       <Route path='/edit-note/:id' element={<EditNote/>} />
     </Routes>
-    </BrowserRouter>
+    </HashRouter>
     </main>
   )
 }
